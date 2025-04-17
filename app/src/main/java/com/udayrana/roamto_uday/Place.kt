29d1b82d553301c5ade366c6a735c6081fb9ac1d
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-17 15:29:19 -0400

    Store date as Long

--- app/src/main/java/com/udayrana/roamto_uday/Place.kt
+++ app/src/main/java/com/udayrana/roamto_uday/Place.kt
@@ -3,14 +3,13 @@ package com.udayrana.roamto_uday
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
-import java.util.Date
 
 @Entity
 data class Place(
     @PrimaryKey(autoGenerate = true) val uid: Int,
     val title: String,
     val description: String,
-    val date: Date,
+    val date: Long,
     val address: String,
     val latitude: Double,
     val longitude: Double
