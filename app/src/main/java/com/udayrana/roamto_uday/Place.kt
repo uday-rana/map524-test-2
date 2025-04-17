5f55966b16e2d0dbe5fe2a4ee8afd6f8378ba6b7
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-17 13:52:22 -0400

    Use java.util.Date and android.location.Location types

--- app/src/main/java/com/udayrana/roamto_uday/Place.kt
+++ app/src/main/java/com/udayrana/roamto_uday/Place.kt
@@ -1,14 +1,16 @@
 package com.udayrana.roamto_uday
 
+import android.location.Location
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
+import java.util.Date
 
 @Entity
 data class Place(
     @PrimaryKey(autoGenerate = true) val uid: Int,
     val title: String,
     val description: String,
-    val date: String, // TODO: pick better types
-    val location: String,
+    val date: Date,
+    val location: Location,
 ) : Serializable
