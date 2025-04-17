bda6720feef4edae43e55b7d9de15e06dffce029
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-17 12:48:36 -0400

    Initial commit

---
+++ app/src/main/java/com/udayrana/roamto_uday/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.udayrana.roamto_uday
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udayrana.roamto_uday.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
