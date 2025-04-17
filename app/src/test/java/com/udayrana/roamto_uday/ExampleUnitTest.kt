42092fbf31231e323ca3848ba6b7394fcda43e9f
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-17 18:53:20 -0400

    Format

--- app/src/test/java/com/udayrana/roamto_uday/ExampleUnitTest.kt
+++ app/src/test/java/com/udayrana/roamto_uday/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.udayrana.roamto_uday
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
